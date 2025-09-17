f91472f44372c80bdf98afe1ea8984ef49b986b2
Omar <olr7742@nyu.edu>
2025-09-17 13:39:19 -0700

    remove tcp

--- pipeline/1_generate_grasps.py
+++ pipeline/1_generate_grasps.py
@@ -898,8 +898,8 @@ def sample_multiple_grasps(
         f"Final result: {len(transforms):,} valid grasps with quality >= {min_quality}"
     )
 
-    for i in range(len(transforms)):
-        transforms[i][:3, 3] += transforms[i][:3, 3] @ gripper.tcp_offset
+    # for i in range(len(transforms)):
+    #     transforms[i][:3, 3] += transforms[i][:3, 3] @ gripper.tcp_offset
 
     return points, normals, transforms, roll_angles, standoffs, collisions, quality
 
