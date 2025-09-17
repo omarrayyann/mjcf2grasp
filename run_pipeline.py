1b2a50605456157e287fbcd2edbb112ce3abe5d9
Omar <olr7742@nyu.edu>
2025-09-17 14:06:44 -0700

    robotiq

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,7 +5,7 @@ import wandb
 from datetime import datetime
 
 USE_WANDB = False
-gripper_name = "panda"
+gripper_name = "robotiq"
 
 with open("matched_objs.json", "r") as f:
     data = json.load(f)
