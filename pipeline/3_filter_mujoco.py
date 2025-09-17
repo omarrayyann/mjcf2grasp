7a980c46628feb7cb33b4f61f90da304824b867d
Omar <olr7742@nyu.edu>
2025-09-17 14:01:30 -0700

    updarte

--- pipeline/3_filter_mujoco.py
+++ pipeline/3_filter_mujoco.py
@@ -388,7 +388,7 @@ def run_simulation_with_viewer(xml_content, object_name, use_viewer):
         for i, (transform, quality) in pbar:
             # transform[:3, 3] += transform[:3, :3] @ gripper.tcp_offset
             # if i < 10:
-                continue
+            # continue
             # transform = np.array(
             #     [
             #         [9.95544306e-01, 7.81320014e-02, 5.27913288e-02, 2.61137130e-04],
