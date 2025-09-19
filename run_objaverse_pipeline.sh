2a087e2e10ee6c72f60eafb5c2f09d64df59fe2b
root <root@neptune-cs-aus-259.reviz.ai2.in>
2025-09-19 05:57:35 +0000

    update shell script for beaker job

--- run_objaverse_pipeline.sh
+++ run_objaverse_pipeline.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+# Change to the script directory to ensure relative paths work
+cd /root/yejink/thor-grasp
+
+# Set default data path if no argument provided
+DATA_PATH=${1:-"/root/mujoco-thor/assets/scenes/procthor-100k-debug/"}
+
+#echo "Using data path: $DATA_PATH"
+#ls -l "$DATA_PATH"
+
+#pip install
+/opt/miniconda3/envs/mjgrasp/bin/pip install -r requirements.txt
+
 # get all objects from objaverse
-/opt/miniconda3/envs/mjgrasp/bin/python scripts/find_objaverse_objects.py ../mujoco-thor/assets/scenes/procthor-100k-debug/ --output objaverse_matched_objs.json
+/opt/miniconda3/envs/mjgrasp/bin/python scripts/find_objaverse_objects.py "$DATA_PATH" --output objaverse_matched_objs.json
 
 # run the pipeline
 /opt/miniconda3/envs/mjgrasp/bin/python run_pipeline.py 
