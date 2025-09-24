142ef96aa6fbf799e1d23504351a6e64b32be94f
Omar <olr7742@nyu.edu>
2025-09-24 02:26:36 -0700

    Updated Robotiq Gripper Mesh

--- grippers/RobotiqGripper.py
+++ grippers/RobotiqGripper.py
@@ -4,14 +4,15 @@ import trimesh.transformations as tra
 
 
 class RobotiqGripper:
-    def __init__(self, q=0.0464, num_contact_points_per_finger=10, root_folder=""):
-        self.default_pregrasp_configuration = 0.0464
+    def __init__(self, q=0.048372, num_contact_points_per_finger=15, root_folder=""):
+        self.default_pregrasp_configuration = 0.048372
 
         if q is None:
             q = self.default_pregrasp_configuration
 
         self.q = q
-        self.tcp_offset = np.array([0, 0, 0.110673])
+        gripping_center = 0.12582
+        self.tcp_offset = np.array([0, 0, gripping_center])
         fn_base = root_folder + "assets/gripper_models/robotiq_gripper/hand.stl"
         fn_finger = root_folder + "assets/gripper_models/robotiq_gripper/finger.stl"
         self.base = trimesh.load(fn_base)
@@ -19,15 +20,15 @@ class RobotiqGripper:
         self.finger_r = self.finger_l.copy()
 
         self.finger_l.apply_transform(tra.euler_matrix(0, 0, np.pi))
-        self.finger_l.apply_translation([+q, 0, 0.110673])
-        self.finger_r.apply_translation([-q, 0, 0.110673])
+        self.finger_l.apply_translation([+q, 0, gripping_center])
+        self.finger_r.apply_translation([-q, 0, gripping_center])
 
         self.fingers = trimesh.util.concatenate([self.finger_l, self.finger_r])
         self.hand = trimesh.util.concatenate([self.fingers, self.base])
 
         self.ray_origins = []
         self.ray_directions = []
-        for i in np.linspace(-0.03, 0.03, num_contact_points_per_finger):
+        for i in np.linspace(-0.022, 0.029, num_contact_points_per_finger):
             self.ray_origins.append(
                 np.r_[self.finger_l.bounding_box.centroid + [0, 0, i], 1]
             )
