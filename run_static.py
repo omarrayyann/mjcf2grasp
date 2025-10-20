c32c32ba95cb9d55cc3bdd2520fff9c05e8203dd
Omar Rayyan <olr7742@nyu.edu>
2025-10-19 23:56:40 -0700

    Update run_static.py

--- run_static.py
+++ run_static.py
@@ -121,7 +121,7 @@ for obj in data:
                           "--max_contact_depth", "1.0",
                           "--min_contact_depth", "0.0",
                           "--center_contact_depth", "0.75",
-                          "--contact_depth_bias", "1.8",
+                          "--contact_depth_bias", "2.8",
                           
                           #"--render", 
                           "--rotate", 
